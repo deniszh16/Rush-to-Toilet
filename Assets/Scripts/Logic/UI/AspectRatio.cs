90b06b1bc9d0cd6f2eeef747c549d4d9a4bed668
Denis Zhursky <zhursky16@gmail.com>
2023-08-05 22:06:53 +0300

    Исправление ошибок, адаптивность UI.

---
+++ Assets/Scripts/Logic/UI/AspectRatio.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Logic.UI
+{
+    [RequireComponent(typeof(Camera))]
+    public class AspectRatio : MonoBehaviour
+    {
+        private Camera _camera;
+        private float _aspectRatio;
+        private const float _cameraSize = 6.2f;
+
+        private void Awake()
+        {
+            _camera = GetComponent<Camera>();
+            _aspectRatio = _camera.aspect;
+        }
+
+        private void Start()
+        {
+            if (_aspectRatio <= 0.5f)
+                _camera.orthographicSize = _cameraSize;
+        }
+    }
+}
